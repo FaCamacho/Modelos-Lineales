4b91b272611b0bfce0409cc1ca4afbfb39ab1ac8
unknown <fabriciocamacho@gmail.com>
2024-04-04 10:40:05 -0300

    a

--- Laboratorio/Laboratorio 1.R
+++ Laboratorio/Laboratorio 1.R
@@ -1,4 +1,30 @@
 library(readxl)
-condados <- read_excel("Laboratorio/condados.xlsx")
+library(tidyverse)
+
+condados <- read_excel("Laboratorio/condados.xlsx",na="NA")
 
 class(condados)
+
+condados<-as.data.frame(condados)
+
+class(condados)
+
+table(condados$estado)
+
+cuenta <- condados %>% group_by(.,estado) %>% count(.) 
+
+
+Florida <- condados %>% filter(.,estado == "Florida")
+
+str(Florida$esp_vida)
+
+#as.numeric(Florida$esp_vida)
+
+ggplot(data=Florida)+geom_histogram(mapping = aes(x=esp_vida))
+
+ggplot(data=Florida)+geom_boxplot(mapping = aes(x=esp_vida))
+
+
+
+
+
